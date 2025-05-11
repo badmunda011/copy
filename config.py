822e6cd24a2ad538d4e162cac9853d392614a625
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-11 11:06:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("25742938"))
-API_HASH = getenv("b35b715fe8dc0a58e8048988286fc5b6")
+API_ID = 25742938
+API_HASH = b35b715fe8dc0a58e8048988286fc5b6
 BOT_TOKEN = getenv("7332934706:AAHke648XmOaIoqv-7gigzzmGrkp8acaff0")
 BOT_USERNAME = getenv("Gcopyright_Bot")
 OWNER_ID = int(getenv("7588172591"))
