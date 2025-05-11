4f82f5f9b1f9e73cc1b36709e24275073ed34f05
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-11 11:03:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_USERNAME = getenv("BOT_USERNAME")
-OWNER_ID = int(getenv("OWNER_ID"))
-LOGGER_ID = int(getenv("LOGGER_ID"))
-MONGO_URL = getenv("MONGO_URL")
+API_ID = int(getenv("25742938"))
+API_HASH = getenv("b35b715fe8dc0a58e8048988286fc5b6")
+BOT_TOKEN = getenv("7332934706:AAHke648XmOaIoqv-7gigzzmGrkp8acaff0")
+BOT_USERNAME = getenv("Gcopyright_Bot")
+OWNER_ID = int(getenv("7588172591"))
+LOGGER_ID = int(getenv("-1002625181470"))
+MONGO_URL = getenv("mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/")
 
-SUDOERS = filters.user([7044783841])
+SUDOERS = filters.user([443809517])
