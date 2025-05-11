dfd970f2a363eab4bc5acd33d05412e73fa40b56
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-05-11 12:06:05 +0530

    Update __init__.py

--- PROTECTOR/__init__.py
+++ PROTECTOR/__init__.py
@@ -1,1 +1,27 @@
+from pyrogram import Client
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telegram.ext import Application
+import config
 
+# Pyrogram Bot Client (With Bot Token)
+app = Client(
+    name="PROTECTOR", 
+    api_id=config.API_ID, 
+    api_hash=config.API_HASH, 
+    bot_token=config.BOT_TOKEN,
+    plugins=dict(root="PROTECTOR.Modules")
+)
+
+# Telethon Bot Client (With Bot Token)
+bot = TelegramClient(
+             session="PROTECTOR", 
+             api_id=config.API_ID, 
+             api_hash=config.API_HASH
+             ).start(bot_token=config.BOT_TOKEN)
+
+
+# Telegram (python-telegram-bot) Client
+application = Application.builder().token(config.BOT_TOKEN).build()
+
+plugins = dict(root="PROTECTOR.Modules")
